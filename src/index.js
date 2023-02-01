94eca86688af754dcecf176dfb5cd779b0f82450
Chuguev_Ivan <Chuguev19930612@gmail.com>
2023-02-02 01:38:06 +0200

    slaider

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 const inistSlider = () => {
     const $slider = $('.carousel-section');
+
     $slider.owlCarousel({
         center: true,
         items: 2,
         loop: false,
         nav: true,
+        dots: false,
         margin: 24,
-        responsiveClass:true,
-        responsive:{
-        0:{
-            items:2,
-            nav:true
-        },
-        1024:{
-            items:3,
-            nav:false
-        },
-        1280:{
-            items:3,
-            nav:true,
-            loop:false
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items:1,
+                nav:true
+            },
+            1024: {
+                items: 2,
+            }
         }
-    }
     });
 };
 
