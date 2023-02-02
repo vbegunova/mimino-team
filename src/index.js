33a019c026d907175c84f4ec335f9323a5009315
vbegunova <veronikabegunova42@gmail.com>
2023-02-02 23:18:45 +0200

    modals

--- src/index.js
+++ src/index.js
@@ -1,132 +1,70 @@
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
-// Script to close and open Table-Modal window
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-table-open]'), 
-      closeModalBtn: document.querySelector('[data-modal-table-close]'),
-      modal: document.querySelector('[data-modal-table]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-     
-      const isModalOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-      refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-      refs.modal.classList.toggle('is-hidden');
-  
-      const scrollLockMethod = !isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body)
-    }
-})();
+document.addEventListener('DOMContentLoaded', function() {
 
-// Script to close and open Room-Modal window
+   /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+   var modalButtons = document.querySelectorAll('.js-open-modal'),
+       overlay      = document.querySelector('.js-overlay-modal'),
+       closeButtons = document.querySelectorAll('.js-modal-close');
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-room-open]'), 
-    closeModalBtn: document.querySelector('[data-modal-room-close]'),
-    modal: document.querySelector('[data-modal-room]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+   /* Перебираем массив кнопок */
+   modalButtons.forEach(function(item){
 
-  function toggleModal() {
-   
-    const isModalOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
+      /* Назначаем каждой кнопке обработчик клика */
+      item.addEventListener('click', function(e) {
 
-    const scrollLockMethod = !isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body)
-  }
-})();
+         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+         e.preventDefault();
 
-// Script to close and open Price-Modal window
+         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+         var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-price-open]'), 
-      closeModalBtn: document.querySelector('[data-modal-price-close]'),
-      modal: document.querySelector('[data-modal-price]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-     
-      const isModalOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-      refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-      refs.modal.classList.toggle('is-hidden');
-  
-      const scrollLockMethod = !isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body)
-    }
-})();
 
-// Script for phone auto editing 
+         /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+         modalElem.classList.add('active');
+         overlay.classList.add('active');
+      }); // end click
 
-  const isNumericInput = event => {
-    const key = event.keyCode;
-    return (
-      (key >= 48 && key <= 57) || // Allow number line
-      (key >= 96 && key <= 105) // Allow number pad
-    );
-  };
+   }); // end foreach
 
-  const isModifierKey = event => {
-    const key = event.keyCode;
-    return (
-      event.shiftKey === true ||
-      key === 35 ||
-      key === 36 || // Allow Shift, Home, End
-      key === 8 ||
-      key === 9 ||
-      key === 13 ||
-      key === 46 || // Allow Backspace, Tab, Enter, Delete
-      (key > 36 && key < 41) || // Allow left, up, right, down
-      // Allow Ctrl/Command + A,C,V,X,Z
-      ((event.ctrlKey === true || event.metaKey === true) &&
-        (key === 65 || key === 67 || key === 86 || key === 88 || key === 90))
-    );
-  };
 
-  const enforceFormat = event => {
-    // Input must be of a valid number format or a modifier key, and not longer than ten digits
-    if (!isNumericInput(event) && !isModifierKey(event)) {
-      event.preventDefault();
-    }
-  };
+   closeButtons.forEach(function(item){
 
-  const formatToPhone = event => {
-    if (isModifierKey(event)) {
-      return;
-    }
+      item.addEventListener('click', function(e) {
+         var parentModal = this.closest('.modal');
 
-    const input = event.target.value.replace(/\D/g, '').substring(0, 12); // First ten digits of input only
-    const areaCodeN = '+38 ';
-    const areaCode = input.substring(2, 5);
-    const middle = input.substring(5, 8);
-    const middleN = input.substring(8, 10);
-    const last = input.substring(10, 12);
+         parentModal.classList.remove('active');
+         overlay.classList.remove('active');
+      });
 
-    if (input.length > 10) {
-      event.target.value = `${areaCodeN}(${areaCode}) ${middle}-${middleN}-${last}`;
-    } else if (input.length > 8) {
-      event.target.value = `${areaCodeN}(${areaCode}) ${middle}-${middleN}`;
-    } else if (input.length > 5) {
-      event.target.value = `${areaCodeN}(${areaCode}) ${middle}`;
-    } else if (input.length > 1) {
-      event.target.value = `${areaCodeN}(${areaCode}`;
-    } else if (input.length > 0) {
-      event.target.value = `${areaCodeN}(0${areaCode}`;
-    }
-  };
+   }); // end foreach
 
-  const inputElement = document.getElementById('phoneNumber');
-  inputElement.addEventListener('keydown', enforceFormat);
-  inputElement.addEventListener('keyup', formatToPhone);
+
+    document.body.addEventListener('keyup', function (e) {
+        var key = e.keyCode;
+
+        if (key == 27) {
+
+            document.querySelector('.modal.active').classList.remove('active');
+            document.querySelector('.overlay').classList.remove('active');
+        };
+    }, false);
+
+
+    overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
+    });
+
+
+
+
+}); // end ready
